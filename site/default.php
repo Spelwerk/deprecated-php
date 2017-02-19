5bafb9381692ab8b55c9336957e97e068535976d
princejonn <jonn@jonn.nu>
2017-02-19 19:51:10 +0100

    framework

--- site/default.php
+++ site/default.php
@@ -8,6 +8,6 @@
 ?>
 
 <div class="sw-l-content__wrap">
-    <h2>This is Spelwerk</h2>
-    <p>Play</p>
+    <h2 class="sw-f-logo">we are spelwerk</h2>
+    <p>This is the webpage of spelwerk. We have designed this system to easily create and play roleplaying games in whatever setting/world we wish. The system is modular and should work with basically whatever you wish to play with.</p>
 </div>
